6e94c5e7d692a97b2836e441b2b71c9260488d6c
popgenomics <camille.roux.1@unil.ch>
2016-06-03 11:32:30 +0200

    figure 3

--- figures/figure_3.R
+++ figures/figure_3.R
@@ -1,27 +1,64 @@
-x = read.table("migHetero_NHomo.txt", h=T)
-x[which(x$FST_avg<0),]$FST_avg = 0
-# plot
-#pdf("figure2.pdf", bg="white")
-dev.new(width=10.5, height=5.5)
-par(mfrow=c(1,2), las=2)
-plot(log10(x$netdivAB_avg), x$FST_avg, pch=16, col=grey(0.5), cex=1.2, ylim=c(0,1), xlim=c(log10(1e-5), log10(1)), xlab = "net synonymous divergence", ylab = "", cex.lab = 1.5, xaxt="n", yaxt="n", cex.axis=1.35)
+library(shape)
+
+x = read.csv("../tables/table_S2.csv", h=T)
+
+tmp = rep(NA, nrow(x))
+
+tmp[which(x$Pongoing_migration_Mhetero_Nhetero>=0.8)] = "Migration"
+tmp[which(x$Pongoing_migration_Mhetero_Nhetero<0.2)] = "NoMigration"
+
+bilanHH = tmp
+
+pmig_HH = x$Pongoing_migration_Mhetero_Nhetero 
+
+# plot MHetero_NHetero
+dev.new(width=9, height=5)
+par(las=2, mar=c(5,4,4,12))
+plot(log10(x$netdivAB_avg), pmig_HH, ylab ="" , xlab = "net synonymous divergence", col = "white", cex.lab = 1.25, xaxt = "n", yaxt = "n", xlim=c(log10(1e-5), log10(1)), cex.axis=1.25)
+par(las=3)
+mtext(side=2, text=expression(P["ongoing migration"]), line=2.25, cex=1.5)
 par(las=1)
 axis(side = 1, at = c(0, -1, -2, -3, -4, -5), labels = c(1, 0.1, 0.01, expression(paste("10"^"-3")), expression(paste("10"^"-4")) , expression(paste("10"^"-5"))), cex.axis=1.25)
-axis(side = 2, cex.axis=1.35)
-par(las=3)
-mtext(side = 2, text = expression(paste("mean", " ", F["ST"], sep=" ")), cex=1.5, line=2.5)
+axis(side = 2, at = c(0, 0.2, 0.4, 0.6, 0.8, 1), cex.axis=1.25)
+
+div_NA = log10(sort(x[is.na(bilanHH),]$netdivAB_avg))
+div_Mig = log10(sort(x[which(bilanHH=="Migration"),]$netdivAB_avg))
+div_NoMig = log10(sort(x[which(bilanHH=="NoMigration"),]$netdivAB_avg))
+
+xA1 = div_NoMig[1]
+xB1 = div_Mig[length(div_Mig)]
+xA2 = div_NA[1]
+xB2 = div_NA[length(div_NA)]
+
+rect(xA1, -1, xB1, 2, col = rgb(0, 0, 0, 0.2), border=NA)
+rect(xA2, -1, xB2, 2, col = rgb(0, 0, 0, 0.2), border=NA)
+abline(h=c(0.2, 0.8), lty=2, lwd=1.25)
 
 par(las=1)
-plot(log10(x$netdivAB_avg), x$FST_std, pch=16, col=grey(0.5), cex=1.2, xlab = "net synonymous divergence", ylab = "", cex.lab = 1.5, xaxt="n", yaxt="n", ylim=c(0,0.5), xlim=c(log10(1e-5), log10(1)), cex.axis=1.25) 
-par(las=1)
-axis(side = 1, at = c(0, -1, -2, -3, -4, -5), labels = c(1, 0.1, 0.01, expression(paste("10"^"-3")), expression(paste("10"^"-4")) , expression(paste("10"^"-5"))), cex.axis=1.35)
-axis(side = 2, cex.axis=1.35)
-par(las=3)
-mtext(side = 2, text = expression(paste("std", " ", F["ST"], sep=" ")), cex=1.5, line=2.5)
+# formes des points en fonction du statu d'espces
+formes = rep(0, nrow(x))
+formes[which(x$species_status_NG == 0)] = 22 
+formes[which(x$species_status_NG == 0.5)] = 22
+formes[which(x$species_status_NG == 1)] = 21
 
+# couleurs en fonction de NHetero, MHetero, NHomo, MHomo.
+couleurs = rep(0, nrow(x))
 
-#dev.off()
+heteroM = apply(x[,c(39, 40, 43, 44, 47, 48)], FUN="sum", MARGIN=1)
 
-dev.print(pdf, "figure3.pdf", bg="white")
+#homoM_homoN: species with homogeneity for both introgression and Ne
+couleurs[which(pmig_HH>= 0.8 & heteroM >= 0.8)] = "purple"
+couleurs[which(pmig_HH>= 0.8 & heteroM <= 0.8)] = "turquoise"
+couleurs[which(pmig_HH<= 0.2)] = "red"
+couleurs[which(pmig_HH> 0.2 & pmig_HH<0.8)] = grey(0.25)
+
+couleurs_contour = couleurs
+couleurs_fond = couleurs
+pNe = apply(x[, grep("v2", colnames(x))], MARGIN=1, FUN="sum")
+couleurs_fond[which(pNe < 0.8)] = rgb(0, 0, 0, 0)
+
+points(log10(x$netdivAB_avg), pmig_HH, pch=formes, col=couleurs_contour, bg=couleurs_fond, cex=1.8, lwd=2.5)
+
+dev.print(pdf, "figure3tmp.pdf", bg="white")
 dev.off()
 
