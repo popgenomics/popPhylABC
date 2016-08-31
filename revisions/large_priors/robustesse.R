992709fc58fdcc239013da6fc8b85d87ee0da119
popgenomics <camille.roux.1@unil.ch>
2016-08-31 18:54:46 +0200

    revisions

--- revisions/large_priors/robustesse.R
+++ revisions/large_priors/robustesse.R
@@ -1,3 +1,6 @@
+library(vioplot)
+setwd("/home/croux/Documents/popPhylABC/figures/popPhylABC/revisions/large_priors")
+
 SI = read.table("SI", h=T)
 
 AM = read.table("AM", h=T)
@@ -8,11 +11,11 @@ SC = read.table("SC", h=T)
 
 PAN = read.table("PAN", h=T)
 
-par(mfrow=c(2,2))
-plot(density(c(0, 1, SI$IM + SI$SC + SI$PAN)), main="SI")
-plot(density(c(0, 1, AM$IM + AM$SC + AM$PAN)), main="AM")
-plot(density(c(0, 1, IM$IM + IM$SC + IM$PAN)), main="IM")
-plot(density(c(0, 1, SC$IM + SC$SC + SC$PAN)), main="SC")
+#par(mfrow=c(2,2))
+#plot(density(c(0, 1, SI$IM + SI$SC + SI$PAN)), main="SI", xlab=expression(P["ongoing migration"]))
+#plot(density(c(0, 1, AM$IM + AM$SC + AM$PAN)), main="AM", xlab=expression(P["ongoing migration"]))
+#plot(density(c(0, 1, IM$IM + IM$SC + IM$PAN)), main="IM", xlab=expression(P["ongoing migration"]))
+#plot(density(c(0, 1, SC$IM + SC$SC + SC$PAN)), main="SC", xlab=expression(P["ongoing migration"]))
 
 mig_given_mig = c(IM$IM + IM$SC + IM$PAN, SC$IM + SC$SC + SC$PAN)
 mig_given_nomig = c(SI$IM + SI$SC + SI$PAN, AM$IM + AM$SC + AM$PAN)
@@ -122,4 +125,47 @@ for(i in 1:nrow(PAN)){
 colnames(res) = c("correctly_infered", "robustness")
 
 PAN = cbind(PAN, res)
+
+
+# robustness as a function of divergence
+tmpSI = cbind(SI$netdivAB_avg, SI$IM, SI$SC, SI$PAN, SI$robustness, SI$correctly_infered)
+tmpAM = cbind(AM$netdivAB_avg, AM$IM, AM$SC, AM$PAN, AM$robustness, AM$correctly_infered)
+tmpIM = cbind(IM$netdivAB_avg, IM$IM, IM$SC, IM$PAN, IM$robustness, IM$correctly_infered)
+tmpSC = cbind(SC$netdivAB_avg, SC$IM, SC$SC, SC$PAN, SC$robustness, SC$correctly_infered)
+tmpPAN = cbind(PAN$netdivAB_avg, PAN$IM, PAN$SC, PAN$PAN, PAN$robustness, PAN$correctly_infered)
+
+res = rbind(tmpSI, tmpAM, tmpIM, tmpSC, tmpPAN)
+colnames(res) = c("netdivAB_avg", "IM", "SC", "PAN", "robustness", "correctly_infered")
+res = as.data.frame(res)
+
+good = which(res$correctly_infered==1)
+pasGood = which(res$correctly_infered==0)
+
+couleurs = vector(length=nrow(res))
+couleurs[good] = "green"
+couleurs[pasGood] = "red"
+
+shapes = vector(length=nrow(res))
+shapes[good] = 1
+shapes[pasGood] = 4
+
+#dev.new(width=9, height=12)
+#par(las=2, mar=c(5,4,1,1), mfrow=c(2, 1))
+plot(log10(res$netdivAB_avg), res$robustness, ylab ="" , xlab = "net synonymous divergence", col = "white", cex.lab = 1.25, xaxt = "n", yaxt = "n", xlim=c(log10(1e-5), log10(1)), cex.axis=1.25)
+par(las=3)
+mtext(side=2, text=expression("robustness"), line=2.5, cex=1.5)
+par(las=1)
+axis(side = 1, at = c(0, -1, -2, -3, -4, -5), labels = c(1, 0.1, 0.01, expression(paste("10"^"-3")), expression(paste("10"^"-4")) , expression(paste("10"^"-5"))), cex.axis=1.25)
+axis(side = 2, at = c(0, 0.2, 0.4, 0.6, 0.8, 1), cex.axis=1.25)
+
+points(log10(res$netdivAB_avg)[good], res$robustness[good], col = couleurs[good], pch = shapes[good])
+points(log10(res$netdivAB_avg)[pasGood], res$robustness[pasGood], col = couleurs[pasGood], pch = shapes[pasGood])
+
+nomigration = na.omit(log10(c(tmpAM[,1], tmpSI[,1])))
+migration = na.omit(log10(c(tmpSC[,1], tmpIM[,1])))
+par(las=3)
+#boxplot(nomigration, migration, names = c("no\nmigration", "migration"), horizontal=T, ylim=c(log10(1e-5), log10(1)), xaxt = "n", xlab = "net synonymous divergence", cex.lab = 1.25, cex.axis = 1.25)
+vioplot(nomigration, migration, names = c("no\nmigration", "migration"), horizontal=T, ylim=c(log10(1e-5), log10(1)), col="white", lwd=2)
+par(las=1)
+axis(side = 1, at = c(0, -1, -2, -3, -4, -5), labels = c(1, 0.1, 0.01, expression(paste("10"^"-3")), expression(paste("10"^"-4")) , expression(paste("10"^"-5"))), cex.axis=1.25)
 
